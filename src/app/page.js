e22373762fb2f56a53e2b6c8edd4eac1a203010f
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2024-03-17 19:30:04 -0400

    Add navbar, services, team, contact, and footer components to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import Navbar from "@/components/navbar";
+import Services from "@/components/services";
+import Team from "@/components/team";
+import ContactUs from "@/components/contact";
+import Footer from "@/components/footer";
+import Head from "next/head";
+import Wellcome from "@/components/wellcome";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="">Hello world!</h1>
-    </main>
+    <>
+      <Head />
+      <div className="flex flex-col min-h-[100dvh] overflow-y-auto">
+        <Navbar />
+        <Wellcome />
+        <main>
+          <Services />
+          <Team />
+          <ContactUs />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
